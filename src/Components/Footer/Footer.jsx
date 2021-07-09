68d4ca608ae182cf5c51ae31ea5dd61e84756ead
Agnieszka Polińska <agapoli@gmail.com>
2021-07-09 13:09:09 +0200

    feat: Header and Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (  
+        <footer id="info" class="p-2">
+            Agnieszka Polińska - Przewodnik po Wrocławiu &copy; 2020 Wszelkie prawa zastrzeżone. 
+	    </footer>
+    );
+}
+ 
+export default Footer;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import logo from '../../images/logo.png';
+import headerLogo from '../../images/logo.png';
 
 import './Header.scss';
 
 const Header = () => {
     return (  
      <header className="header">
-        <img src={logo} className="header__logo" alt="logo" />
-        <h1 className="header__title fw-light">Przewodniczka po Wrocławiu</h1>
+        <img src={headerLogo} className="header__logo" alt="logo" />
+        <h1 className="header__title fw-light">Przewodniczka po&nbsp;Wrocławiu</h1>
      </header>
     );
 }
