0ac2b1242945c36e4544134fc0459dece4bf1e08
Agnieszka Polińska <agapoli@gmail.com>
2021-07-09 13:07:47 +0200

    feat: Button

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.scss';
+
+const Button = () => {
+    return (  
+        <button type="submit" className="btn button button--center px-4">Zapraszam do quizu!</button>
+    );
+}
+ 
+export default Button;
