615af88bbdfaef22c8adb465e44dadc835b31362
Agnieszka Polińska <agapoli@gmail.com>
2021-07-13 18:45:52 +0200

    Toggle mobile menu

--- src/Components/Navigation/Menu.jsx
+++ src/Components/Navigation/Menu.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
+import {useMediaQuery} from 'react-responsive';
+
 import './Menu.scss';
 
 const Menu = () => {
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const isNotMobileMenu = useMediaQuery({ query: '(min-width:576px)' });
+
+    const toggleMenu = () => setIsMenuVisible(prev => !prev);
+
     return (  
         <aside className="menu">
             <nav className="navbar navbar-dark menu__toggler">
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" onClick={toggleMenu}>
                         <span className="navbar-toggler-icon"></span>
                     </button>
                 </div>
             </nav>
-            <ul className="collapse show menu__list" id="navbarToggleExternalContent">
-                <li class="nav-item">
+            <ul className={`collapse ${isNotMobileMenu | isMenuVisible && 'show'} menu__list`} id="navbarToggleExternalContent">
+                <li className="nav-item">
 					<a className="nav-link menu__link" id="about-me" href="#">O mnie</a>
 				</li>
 				<li className="nav-item">
