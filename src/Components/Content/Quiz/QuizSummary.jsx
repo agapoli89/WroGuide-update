53abe5e6ad2d057c21e6ffb453474812db8ff296
Agnieszka Polińska <agapoli@gmail.com>
2021-07-20 16:14:34 +0200

    feat: QuizSummary

--- src/Components/Content/Quiz/QuizSummary.jsx
+++ src/Components/Content/Quiz/QuizSummary.jsx
@@ -1,10 +1,17 @@
 import Button from '../../Button/Button';
 
 const QuizSummary = ({click, score}) => {
+    const comment = score <= 2
+        ? "Zdecydowanie przyda Ci się wycieczka po Wrocławiu z przewodnikiem :) Zapraszam!"
+        : score === 3
+            ? "Widać, że masz sporą wiedzę o Wrocławiu, ale wciąż znajdzie się parę ciekawostek, które mogą Cię zainteresować. Zapraszam na wycieczkę!"
+            : "Gratulacje! Twoja wiedza o Wrocławiu jest imponująca! Czas zastanowić się nad zrobieniem kursu na przewodnika :)"
+
     return (  
         <>
             <p>Twój wynik: {score}/5</p>
-            <Button text="Spróbuj jeszcze raz" click={click} />
+            <p>{comment}</p>
+            <Button text="Spróbuj jeszcze raz" click={click} /> 
         </>
     );
 }
